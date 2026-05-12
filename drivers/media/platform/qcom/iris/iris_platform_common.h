ca30d32d9008220f3a2464f546ef294a46a4ef2a
Wangao Wang <wangao.wang@oss.qualcomm.com>
2026-05-12 16:55:11 +0800

    BACKPORT: media: qcom: iris: Add Long-Term Reference support for encoder
    Add Long-Term Reference(LTR) frame support for both gen1 and gen2
    encoders by enabling the following V4L2 controls:
    V4L2_CID_MPEG_VIDEO_LTR_COUNT
    V4L2_CID_MPEG_VIDEO_USE_LTR_FRAMES
    V4L2_CID_MPEG_VIDEO_FRAME_LTR_INDEX
    
    Link: https://lore.kernel.org/all/20260512-batch2_features-v7-2-4954e3b4df84@oss.qualcomm.com/
    Tested-by: Neil Armstrong <neil.armstrong@linaro.org> # on SM8650-HDK
    Reviewed-by: Dikshita Agarwal <dikshita.agarwal@oss.qualcomm.com>
    Signed-off-by: Wangao Wang <wangao.wang@oss.qualcomm.com>
    Signed-off-by: Bryan O'Donoghue <bod@kernel.org>
    (cherry picked from commit d7574042c58b571131b4f8da1491ceaf8bba1246)

--- drivers/media/platform/qcom/iris/iris_platform_common.h
+++ drivers/media/platform/qcom/iris/iris_platform_common.h
@@ -28,6 +28,9 @@ struct iris_inst;
 #define MAX_QP_HEVC				63
 #define DEFAULT_QP				20
 #define BITRATE_DEFAULT			20000000
+#define INVALID_DEFAULT_MARK_OR_USE_LTR		-1
+#define MAX_LTR_FRAME_COUNT_GEN1		4
+#define MAX_LTR_FRAME_COUNT_GEN2		2
 
 #define VIDEO_REGION_SECURE_FW_REGION_ID	0
 #define VIDEO_REGION_VM0_SECURE_NP_ID		1
@@ -160,6 +163,9 @@ enum platform_inst_fw_cap_type {
 	VFLIP,
 	IR_TYPE,
 	IR_PERIOD,
+	LTR_COUNT,
+	USE_LTR,
+	MARK_LTR,
 	INST_FW_CAP_MAX,
 };
 
